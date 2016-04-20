9fbec1b289248a70539ce7db1c213e49ebe5b785
rakakurnian <rakha7x@yahoo.com>
2016-04-20 18:52:12 +0700

    Update myprofile.php

--- myprofile.php
+++ myprofile.php
@@ -6,7 +6,7 @@ $profile = mysql_fetch_array($getProfile);
 <!DOCTYPE html>
 <html>
 <head>
-<title>E-MEUBEL a Interior & Furniture Category Flat Bootstarp responsive Website Template | Contact :: w3layouts</title>
+<title>E-MEUBEL | Interior & Furniture | Profile</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Majestic Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
@@ -121,7 +121,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<div class="container">
 			<div class="footer-top">
 				<div class="col-md-4 footer-left">
-					<p>Design by <a href="http://w3layouts.com/" target="_blank"> W3layouts</a></p>
+					<p>Design by Raka Kurnia N</p>
 				</div>
 				<div class="col-md-4 footer-left">
 					<h3>Follow Us</h3>
@@ -134,9 +134,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</div>
 				<div class="col-md-4 footer-left">
 					<h3>Address</h3>
-					<p>The company name, 
-					<span>Lorem ipsum dolor,</span>
-					Glasglow Dr 40 Fe 72.</p>
+					<p>RKN Company 
+					<span>Jalan Leuser 28</span>
+					(0341) 331 757</p>
 				</div>
 				<div class="clearfix"></div>
 			</div>
