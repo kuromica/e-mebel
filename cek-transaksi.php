8af6e68dae647f68d09891d54589d3df860cf5bc
rakakurnian <rakha7x@yahoo.com>
2016-04-21 10:43:27 +0700

    Update cek-transaksi.php

--- cek-transaksi.php
+++ cek-transaksi.php
@@ -6,5 +6,6 @@ $getProduk = mysql_query("select * from produk where id='$id'");
 $produk = mysql_fetch_array($getProduk);
 $tanggal = date('Y-m-d');
 $insert = mysql_query("INSERT INTO transaksi (id_produk,username,harga,status_order,tanggal) VALUES ('1','firman','90000','-','2013-03-03')");
-header('location:cart.php');
+//header('location:cart.php');
+echo "berhasil";
 ?>
