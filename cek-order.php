487c0329c3825f94870a54a9e44ccbf64d91efaf
rakakurnian <rakha7x@yahoo.com>
2016-04-21 11:43:02 +0700

    Update cek-order.php

--- cek-order.php
+++ cek-order.php
@@ -16,7 +16,7 @@ $bukti=$_POST['bukti'];
 			if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){
 				if($ukuran < 1044070){			
 					move_uploaded_file($file_tmp, 'file/'.$nama);
-					$query = mysql_query("UPDATE transaksi set status_order='pending',nama_penerima='$nama', alamat_pengiriman='$alamat' where id_transaksi='$id'");
+					$query = mysql_query("UPDATE transaksi set status_order='pending',nama_penerima='$nama', alamat_pengiriman='$alamat', bukti='$nama' where id_transaksi='$id'");
 					if($query){
 						echo 'FILE BERHASIL DI UPLOAD';
 					}else{
@@ -28,6 +28,5 @@ $bukti=$_POST['bukti'];
 			}else{
 				echo 'EKSTENSI FILE YANG DI UPLOAD TIDAK DI PERBOLEHKAN';
 			}
-//header('location:cart.php');
-echo "$nama"
+header('location:cart.php');
 ?>
