54099210361e81b594a5bd35deff08ece2558ee7
rakakurnian <rakha7x@yahoo.com>
2016-04-21 10:42:10 +0700

    Update cek-transaksi.php

--- cek-transaksi.php
+++ cek-transaksi.php
@@ -5,6 +5,6 @@ $username = $_COOKIE['username'];
 $getProduk = mysql_query("select * from produk where id='$id'");
 $produk = mysql_fetch_array($getProduk);
 $tanggal = date('Y-m-d');
-$insert = mysql_query("INSERT INTO transaksi (id_produk,username,harga,status_order,tanggal) VALUES ('$id','$username','$produk[harga]','-','$tanggal')");
+$insert = mysql_query("INSERT INTO transaksi (id_produk,username,harga,status_order,tanggal) VALUES ('1','firman','90000','-','2013-03-03')");
 header('location:cart.php');
 ?>
